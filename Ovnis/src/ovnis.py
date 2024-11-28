f07c7d361873593ef37a581a160cc10cded397e1
mibullfire <mibullfire@mail.com>
2024-11-28 12:30:21 +0100

    cambios en la estructura

--- Ovnis/src/ovnis.py
+++ Ovnis/src/ovnis.py
@@ -2,108 +2,240 @@ import csv
 from collections import namedtuple, Counter, defaultdict
 from datetime import *
 from typing import Optional
+import math # La he importado para el cálculo de la raíz cuadrada en la función distancia
 
-Ovni = namedtuple('Ovni', 'fechahora, ciudad, estado, forma, duracion, comentarios, latitud, longitud')
-#Coordenada = namedtuple('Coordenada', 'latitude,longitude')
+# Creación de la namedtuple para guardar los archivos, se puede usar tanto la de collections como la de typing
+Ovni = namedtuple('Ovni', 'fechahora, ciudad, estado, forma, duracion, comentarios, coordenadas')
+# Namedtuple auxiliar para guardar las coordenadas
+Coordenadas = namedtuple('Coordenadas', 'latitud, longitud')
+
 def parse_datetime(str):
+    '''
+    Función que recibe un string con el formato 'mm/dd/yyyy hh:mm' y lo convierte a un objeto datetime
+    '''
     return datetime.strptime(str, '%m/%d/%Y %H:%M')
 
 # 1. Función de lectura de datos
 def leer_fichero(fichero):
+    '''
+    Función que recibe un fichero csv y devuelve una lista de objetos Ovni
+    '''
     res = []
     with open(fichero, encoding='utf-8') as f:
         lector = csv.reader(f)
         next(lector)
         for datetime,city,state,shape,duration,comments,latitude,longitude in lector:
-            res.append(Ovni(parse_datetime(datetime),city,state,shape,int(duration),comments,float(latitude),float(longitude)))
+            coordenadas = Coordenadas(float(latitude), float(longitude))
+            res.append(Ovni(parse_datetime(datetime),city,state,shape,int(duration),comments,coordenadas))
     return res
 
 # 2. Operaciones de filtrado, conteo y suma
 def numero_avistamientos_fecha(lista:list, fecha:datetime.date)->int:
-    return len([i for i in lista if i.fechahora.date() == fecha])
+    '''
+    Función que obtiene el número total de avistamientos que se han producido en una fecha determinada, dada por su día, mes y año. 
+    Se contarán, por tanto, los avistamientos que hayan tenido lugar a cualquier hora del día. La función recibe una lista de namedtuple 
+    de tipo Avistamiento, y una fecha de tipo datetime.date.
+    '''
+    # Nota: para funciones simples, puedes elegir entre usar un bucle for o una comprensión de listas con un filtro.
+    filtrado = filter(lambda x: x.fechahora.date() == fecha, lista)
+    return len(list(filtrado))  
+    #return len([i for i in lista if i.fechahora.date() == fecha])
 
 def formas_estados(lista:list, estados:set)->int:
+    '''
+    Función que obtiene el número de formas distintas que presentaron los avistamientos observados en uno o varios estados. 
+    La función recibe una lista de namedtuple de tipo Avistamiento, y un conjunto de estados de tipo _str_.
+    '''
     return len(set(i.forma for i in lista if i.estado in estados))
 
 def duracion_total(lista:list, estado:str)->int:
-    return sum(i.duracion for i in lista if i.estado == estado)
+    '''
+    Función que devuelve la duración total en segundos de los avistamientos que se han observado en un estado. 
+    La función recibe una lista de namedtuple de tipo Avistamiento, y un estado de tipo _str_.
+    '''
+    return sum([i.duracion for i in lista if i.estado == estado])
 
-Cordenadas = namedtuple('Cordenadas', 'latitud, longitud')
-
-import math
-
-def distancia(punto1:Cordenadas, punto2:Cordenadas)->float:
+def distancia(punto1:Coordenadas, punto2:Coordenadas)->float:
+    '''
+    Función que calcula la distancia euclidea entre dos coordenadas. La función recibe dos tuplas de tipo _(float, float)_.
+    '''
     return float(math.sqrt((punto1.latitud - punto2.latitud)**2 + (punto1.longitud - punto2.longitud)**2))
 
-punto1 = Cordenadas(0, 0)
-punto2 = Cordenadas(40.1933333, -85.3863889)
-print(distancia(punto1, punto2)<1000)  
-
-def prueba(lista):
-    return [Cordenadas(i.latitud, i.longitud) for i in lista][0]
-
-def avistamientos_cercanos_ubicacion(lista:list, ubicacion:Cordenadas, distancia_max:float)->list:
-    return [i for i in lista if distancia(Cordenadas(i.latitud, i.longitud), ubicacion) <= distancia_max]
+def avistamientos_cercanos_ubicacion(lista:list[Ovni], ubicacion:Coordenadas, distancia_max:float)->list:
+    return [i for i in lista if distancia(i.coordenadas, ubicacion) <= distancia_max]
 
 # 3. Operaciones con máximos, mínimos y ordenación
 def avistamiento_mayor_duracion(lista:list, forma:Optional[str]=None)->Ovni:
+    '''
+    Función que obtiene el avistamiento de mayor duración de entre todos los avistamientos que tienen una forma determinada.
+    La función recibe una lista de namedtuple de tipo Avistamiento y la forma del avistamiento de tipo _str_.
+    '''
     return max([i for i in lista if (forma == None or i.forma == forma)], key = lambda x: x.duracion)
 
-def avistamiento_cercano_mayor_duracion(lista, ubicacion, distancia_max):
+def avistamiento_cercano_mayor_duracion(lista:list[Ovni], ubicacion:Coordenadas, distancia_max:float)->Ovni:
+    '''
+    Función que devuelve el avistamiento que más tiempo ha durado de aquellos situados dentro de un radio de distancia de una ubicación dada; 
+    es decir, la distancia entre las coordenadas del avistamiento y las coordenadas que se pasan como parámetro de entrada debe ser menor al 
+    radio que también aparece como parámetro de la función. El resultado debe ser una tupla de la forma (duración, comentarios). 
+    La función recibe una lista de namedtuple de tipo Avistamiento, una coordenada que será una tupla de tipo _(float, float)_, 
+    y una distancia de tipo float.
+    '''
+    # Vamos a reusar la función avistamientos_cercanos_ubicacion, y vamos a sacar el máximo de esa lista
     return max(avistamientos_cercanos_ubicacion(lista, ubicacion, distancia_max), key=lambda x: x.duracion)
 
-def avistamientos_fechas(lista, fecha1, fecha2):
-    return [i for i in lista if fecha1 <= i.fechahora.date() <= fecha2]
+def avistamientos_fechas(lista:list[Ovni], fecha1:Optional[date]=None, fecha2:Optional[date]=None)->list[Ovni]:
+    '''
+    Función que devuelve una lista con los avistamientos observados entre una fecha inicial y una fecha final, ambas inclusive. 
+    La lista devuelta estará ordenada de los avistamientos más recientes a los más antiguos. Si la fecha inicial es _None_, 
+    se devolverán todos los avistamientos desde el más antiguo hasta la fecha final. Si la fecha final es _None_, se devolverán todos 
+    los avistamientos desde la fecha inicial hasta el más reciente. Si ambas fechas son _None_, se devolverá la lista de avistamientos completa. 
+    La función recibe una lista de namedtuple de tipo Avistamiento, y dos fechas de tipo datetime.date: una como principio del intervalo y otra como final. 
+    Las fechas recibidas como parámetro tendrán como valor por defecto _None_.
+    '''
+    entre_fechas = list(filter(lambda x: (fecha1 == None or x.fechahora.date() >= fecha1) and (fecha2 == None or x.fechahora.date() <= fecha2), lista))
+    return sorted(entre_fechas, key=lambda x: x.fechahora)
 
 def comentario_mas_largo(lista:list[Ovni], año:int, palabra:str)->Ovni:
-    return max(
-        [i for i in lista if i.fechahora.year == año and palabra in i.comentarios],
-        key=lambda x: len(x.comentarios)
-    )
+    '''
+    Función que devuelve el avistamiento con el comentario más largo, de entre todos los avistamientos observados en un año dado 
+    y cuyo comentario incluye una palabra concreta. La función recibe una lista de namedtuple de tipo Avistamiento, 
+    un año de tipo int, y una palabra de tipo _str_.
+    '''
+    filtrado = list(filter(lambda x: x.fechahora.year == año and palabra in x.comentarios, lista))
+    return max(filtrado, key=lambda x: len(x.comentarios))
 
-def duracion_maxima(lista):
-    return max(lista, key=lambda x: x.duracion)
-
-def media_dias_entre_avistamientos(lista:list[Ovni], año:int)->float:
-    agua = sorted([i.fechahora.date() for i in lista if i.fechahora.year == año])
-    pares = zip(agua, agua[1:])
-    return sum((j - i).days for i, j in pares) / len(agua) - 1
-    # fechas = sorted([i.fechahora.date() for i in lista if i.fechahora.year == año])
-    # if len(fechas) < 2:
-    #     return 0.0
-    # else:
-    #     return sum((fechas[i] - fechas[i-1]).days for i in range(1, len(fechas))) / (len(fechas) - 1)
+def media_dias_entre_avistamientos(lista:list[Ovni], año:Optional[date]=None)->float:
+    '''
+    Función que devuelve la media de días transcurridos entre dos avistamientos consecutivos en el tiempo. La función permite hacer el cálculo para todos 
+    los avistamientos, o solo para los de un año concreto. La función recibe una lista de namedtuple de tipo Avistamiento y un año de tipo int.
+    '''
+    # Filtra las fechas de los avistamientos que ocurrieron en el año especificado y las ordena cronológicamente
+    fechas = sorted([i.fechahora.date() for i in lista if año == None or i.fechahora.year == año])
+    # Crea pares de fechas consecutivas
+    pares = zip(fechas[:-1], fechas[1:])
+    # Calcula la diferencia en días entre cada par de fechas consecutivas con una comprensión de listas
+    pares = list(map(lambda par: (par[1]-par[0]).days, pares))
+    # Calcula y devuelve la media de días entre avistamientos consecutivos
+    return sum(pares)/len(pares)
 
 # 4. Operaciones con diccionarios
-def avistamientos_por_fecha(lista):
-    return {i.fechahora.date(): len([j for j in lista if j.fechahora.date() == i.fechahora.date()]) for i in lista}
+def avistamientos_por_fecha(lista:list[Ovni])->dict[datetime.date, list[Ovni]]:
+    '''
+    Función que crea un diccionario que relaciona las fechas con los avistamientos observados en dichas fechas. 
+    Es decir, un diccionario cuyas claves son las fechas y cuyos valores son los conjuntos de avistamientos observados en cada fecha. 
+    La función recibe una lista de namedtuple de tipo Avistamiento.
+    '''
+    diccionario = defaultdict(list)
+    for i in lista:
+        diccionario[i.fechahora.date()].append(i)
+    return diccionario
 
-def formas_por_mes(lista):
-    return {i: len([j for j in lista if j.fechahora.month == i]) for i in range(1, 13)}
+def formas_por_mes(lista: list[Ovni]) -> dict[str, set[str]]:
+    '''
+    Función que devuelve un diccionario que indexa las distintas formas de avistamientos por los nombres de los meses en que se observaron.
+    Para cada mes (por nombre), se tendrá un conjunto con todas las formas distintas observadas en dicho mes.
+    La función recibe una lista de namedtuple de tipo Ovni.
+    '''
+    meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", 
+             "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+    
+    diccionario = defaultdict(set)
+    for avistamiento in lista:
+        # Obtiene el nombre del mes basándose en el índice del mes (restando 1 porque los índices de listas empiezan en 0)
+        mes_nombre = meses[avistamiento.fechahora.month - 1]
+        diccionario[mes_nombre].add(avistamiento.forma)
+    
+    return diccionario
 
-#...
+def numero_avistamientos_por_año(lista: list[Ovni]) -> dict[int, int]:
+    '''
+    Función que crea un diccionario que relaciona cada año con el número de avistamientos observados en dicho año. 
+    Es decir, un diccionario cuyas claves son los años y cuyos valores son el número de avistamientos observados en cada año. 
+    La función recibe una lista de namedtuple de tipo Avistamiento.
+    '''
+    diccionario = Counter(map(lambda x: x.fechahora.year, lista))
+    return dict(sorted(diccionario.items()))
+
+def coordenadas_mas_avistamientos(lista:list[Ovni])->None:
+    '''
+    Función que devuelve las coordenadas enteras que se corresponden con la zona donde más avistamientos se han observado. 
+    Por ejemplo, si hay avistamientos en las coordenadas (40.1, -85.3), (41.13, -85.1) y (40.2, -85.4), la zona con más avistamientos 
+    corresponde a las coordenadas enteras (40, -85) con 2 avistamientos.
+    '''
+    dicc = defaultdict(lambda: 0)
+    for i in lista:
+        dicc[i.coordenadas] += 1
+    
+    return max(dicc.items(), key = lambda x: x[1])
 
 def hora_mas_avistamientos(lista:list[Ovni])->int:
+    '''
+    Función que devuelve la hora del día (de 0 a 23) en la que se han observado un mayor número de avistamientos.
+    '''
     horas = map(lambda x: x.fechahora.hour, lista)
     c = Counter(horas)
     lista_maximos:list[tuple[int,int]] = c.most_common(1)
     tupla:tuple[int,int] = lista_maximos[0]
     return tupla[0], tupla[1]
 
-## Extra, mismo código pero con una lista de mas comunes
-def hora_mas_avistamientos2(lista:list[Ovni])->int:
-    horas = map(lambda x: x.fechahora.hour, lista)
-    c = Counter(horas)
-    frecuencia_maxima = c.most_common(1)[0][1]
-    return list(filter(lambda x: x[1] == frecuencia_maxima, c.items()))
-
-def coordenadas_mas_avistamientos(lista:list[Ovni])->None:
-    dicc = defaultdict(lambda: 0)
+def longitud_media_comentarios_por_estado(lista:list[Ovni])->dict[str, float]:
+    '''
+    Función que devuelve un diccionario en el que las claves son los estados donde se producen los avistamientos, 
+    y los valores son la longitud media de los comentarios de los avistamientos observados en cada estado.
+    '''
+    dicc = defaultdict(lambda: [])
     for i in lista:
-        coordenada = Cordenadas(int(i.latitud), int(i.longitud))
-        dicc[coordenada] += 1
-    
-    return max(dicc.items(), key = lambda x: x[1])
+        dicc[i.estado].append(len(i.comentarios))
+    return {k: sum(v)/len(v) for k,v in dicc.items()}
+
+def porc_avistamientos_por_forma(lista:list[Ovni])->dict[str, float]:
+    pass
+
+def avistamientos_mayor_duracion_por_estado(lista:list[Ovni])->dict[str, Ovni]:
+    pass
+
+def año_mas_avistamientos_forma(lista:list[Ovni], forma:str)->int:
+    lista = filter(lambda x: x.forma == forma, lista)
+    contador = Counter(map(lambda x: x.fechahora.year, lista))
+    return contador.most_common(1)[0][0]
+
+def estados_mas_avistamientos(lista:list[Ovni])->list[str]:
+    pass
+
+def duracion_total_avistamientos_año(lista:list[Ovni], año:int)->int:
+    pass
+
+def avistamiento_mas_reciente_estado(lista:list[Ovni], estado:str)->Ovni:
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Funciones extras que no estan en el md:
 
 def año_con_mayor_duracion_total_avistamientos(lista:list[Ovni])->int:
     dicc = defaultdict(lambda: 0)
@@ -111,5 +243,16 @@ def año_con_mayor_duracion_total_avistamientos(lista:list[Ovni])->int:
         dicc[i.fechahora.year] += i.duracion
     return max(dicc.items(), key = lambda x: x[1])[0]
 
-def top_n_avistamientos_mas_largos_por_año(lista:list[Ovni], n:int=3)->dict[int, list[Ovni]]:
-    pass
+def numero_avistamientos_por_dia(lista:list[Ovni])->dict[datetime.date, int]:
+    contador = Counter(map(lambda x: x.fechahora.date(), lista))
+    return contador
+
+def estado_con_suma_top_n_duraciones_mas_larga(lista: list[Ovni], n: int = 3) -> str:
+    res = defaultdict(list)
+    for i in lista:
+        res[i.estado].append(i.duracion)
+    sumas = {
+        estado: sum(sorted(duraciones, reverse=True)[:n])
+        for estado, duraciones in res.items()
+    }
+    return max(sumas.items(), key=lambda x: x[1])[0]
--- Ovnis/src/test.py
+++ Ovnis/src/test.py
@@ -1,80 +1,99 @@
 from ovnis import *
 from colorama import Fore
 
-def test_leer_fichero(fichero):
+# 1. Función de lectura de datos
+def test_leer_fichero(fichero:str)->None:
     lista = leer_fichero(fichero)
     print(Fore.GREEN + '\nTest Leer fichero')
     print('Número de avistamientos:', len(lista))
     print('Primer avistamiento:', lista[0])
 
-def test_numero_avistamientos_fecha(fichero):
-    lista = leer_fichero(fichero)
+# 2. Operaciones de filtrado, conteo y suma
+def test_numero_avistamientos_fecha(lista:list[Ovni], fecha:date)->None:
     print(Fore.LIGHTMAGENTA_EX + '\nNúmero de avistamientos en una fecha')
-    print('Número de avistamientos en 2004-10-23:', numero_avistamientos_fecha(lista, date(2010, 12, 3)))
+    print('Número de avistamientos en 2004-10-23:', numero_avistamientos_fecha(lista, fecha))
 
-def test_formas_estados(fichero):
-    avistamientos = leer_fichero(fichero)
-    estados = {'in', 'nm'}
+def test_formas_estados(lista:list[Ovni], estados:set[str])->None:
     print(Fore.BLUE + '\nFormas en los estados')
     print('Número de formas en los estados', estados)
-    print(formas_estados(avistamientos, estados))
+    print(formas_estados(lista, estados))
 
-def test_avistamientos_cercanos_ubicacion(fichero):
-    avistamientos = leer_fichero(fichero)
-    ubicacion = Cordenadas(0, 0)
-    distancia = 70
+def test_duracion_total(lista:list[Ovni], estado:str)->None:
+    print(Fore.RED + '\nDuración total')
+    print('Duración total en el estado', estado)
+    print(duracion_total(lista, estado))
+
+def test_distancia()->None:
+    punto1 = Coordenadas(0, 0)
+    punto2 = Coordenadas(40.1933333, -85.3863889)
+    print(Fore.YELLOW + '\nDistancia entre dos puntos')
+    print('Distancia entre', punto1, 'y', punto2)
+    print(distancia(punto1, punto2))
+
+def test_avistamientos_cercanos_ubicacion(lista:list[Ovni], ubicacion:Coordenadas, distancia:float)->None:
     print(Fore.YELLOW + '\nAvistamientos cercanos a una ubicación')
     print('Avistamientos cercanos a', ubicacion, 'a una distancia de', distancia)
-    print(avistamientos_cercanos_ubicacion(avistamientos, ubicacion, distancia))
+    print(avistamientos_cercanos_ubicacion(lista, ubicacion, distancia))
 
-def test_prueba(fichero):
-    avistamientos = leer_fichero(fichero)
-    print(prueba(avistamientos))
-
-def test_duracion_maxima(fichero):
-    avistamientos = leer_fichero(fichero)
+# 3. Operaciones con máximos, mínimos y ordenación
+def test_avistamiento_mayor_duracion(lista:list[Ovni], forma:str)->None:
     print(Fore.RED + '\nDuración máxima')
-    print('Duración máxima')
-    print(duracion_maxima(avistamientos))
+    print('Avistamiento con la duración máxima')
+    print(avistamiento_mayor_duracion(lista, forma))
 
-def test_avistamiento_cercano_mayor_duracion(fichero):
-    avistamientos = leer_fichero(fichero)
-    ubicacion = Cordenadas(0, 0)
-    distancia = 70
+def test_avistamiento_cercano_mayor_duracion(lista:list[Ovni], ubicacion:Coordenadas, distancia:float)->None:
     print(Fore.CYAN + '\nAvistamiento cercano con mayor duración')
     print('Avistamiento cercano con mayor duración')
-    print(avistamiento_cercano_mayor_duracion(avistamientos, ubicacion, distancia))
+    print(avistamiento_cercano_mayor_duracion(lista, ubicacion, distancia))
 
-def test_avistamiento_mayor_duracion(fichero, forma=None):
-    avistamientos = leer_fichero(fichero)
-    print(Fore.MAGENTA + '\nAvistamiento con mayor duración de x forma',)
-    print('Avistamiento con mayor duración de forma', forma)
-    print(avistamiento_mayor_duracion(avistamientos, forma))
+def test_avistamientos_fechas(lista:list[Ovni], fecha:Optional[date]=None, fecha2:Optional[date]=None)->None:
+    print(Fore.GREEN + '\nAvistamientos en fechas')
+    print('Avistamientos entre las fechas', fecha, 'y', fecha2)
+    print(avistamientos_fechas(lista, fecha, fecha2))
 
-def test_comentario_mas_largo(fichero, año, palabra):
-    avistamientos = leer_fichero(fichero)
-    print(Fore.GREEN + '\nComentario más largo')
-    print('Comentario más largo')
-    print(comentario_mas_largo(avistamientos, año, palabra))
+def test_comentario_mas_largo(lista:list[Ovni], año:int, palabra:str)->None:
+    print(Fore.LIGHTWHITE_EX + '\nComentario más largo')
+    print('Comentario más largo en el año', año, 'que contiene la palabra', palabra)
+    print(comentario_mas_largo(lista, año, palabra))
 
-def test_media_dias_entre_avistamientos(fichero, año):
-    avistamientos = leer_fichero(fichero)
+def test_media_dias_entre_avistamientos(lista:list[Ovni], año:int)->None:
     print(Fore.LIGHTMAGENTA_EX + '\nMedia de días entre avistamientos')
-    print('Media de días entre avistamientos')
-    print(media_dias_entre_avistamientos(avistamientos, año))
+    print('Media de días entre avistamientos del año', año)
+    print(media_dias_entre_avistamientos(lista, año))
 
-def test_hora_mas_avistamientos(fichero):
-    avistamientos = leer_fichero(fichero)
+# 4. Operaciones con Diccionarios
+def test_avistamientos_por_fecha(lista:list[Ovni])->None:
+    print(Fore.LIGHTGREEN_EX + '\nAvistamientos por fecha')
+    print(avistamientos_por_fecha(lista))
+
+def test_formas_por_mes(lista:list[Ovni])->None:
+    print(Fore.LIGHTWHITE_EX + '\nFormas por mes')
+    print(formas_por_mes(lista))
+
+def test_numero_avistamientos_por_año(lista:list[Ovni])->None:
+    print(Fore.LIGHTYELLOW_EX + '\nNúmero de avistamientos por año')
+    print(numero_avistamientos_por_año(lista))
+
+def test_coordenada_mas_avistamientos(lista:list[Ovni])->None:
+    print(Fore.LIGHTBLUE_EX + '\nCoordenada con más avistamientos')
+    print(coordenadas_mas_avistamientos(lista))
+
+def test_hora_mas_avistamientos(lista):
     print(Fore.BLUE + '\nTest Hora con más avistamientos')
-    hora, numero = hora_mas_avistamientos(avistamientos)
+    hora, numero = hora_mas_avistamientos(lista)
     print(f'La hora con más avistamientos es {hora} con {numero} avistamientos')
 
-def test_hora_mas_avistamientos2(fichero):
-    avistamientos = leer_fichero(fichero)
-    print(Fore.BLUE + '\nTest Hora con más avistamientos 2')
-    x = hora_mas_avistamientos2(avistamientos)
-    print(x)
+def test_longitud_media_comentarios_por_estado(lista):
+    print(Fore.MAGENTA + '\ntest_longitud_media_comentarios_por_estado')
+    print(longitud_media_comentarios_por_estado(lista))
 
+def test_porc_avistamientos_por_forma(lista):
+    print(Fore.MAGENTA + '\ntest_porc_avistamientos_por_forma')
+    print(porc_avistamientos_por_forma(lista))
+
+
+
+### A partir de aqui son funciones que tengo que cambiar para que funcionen con los nuevos cambios
 def test_coordenadas_mas_avistamientos(fichero):
     avistamientos = leer_fichero(fichero)
     print(Fore.CYAN + '\nTest Coordenadas con más avistamientos')
@@ -87,21 +106,59 @@ def test_año_con_mayor_duracion_total_avistamientos(fichero):
     x = año_con_mayor_duracion_total_avistamientos(avistamientos)
     print(x)
 
+def test_numero_avistamientos_por_dia(fichero):
+    avistamientos = leer_fichero(fichero)
+    print(Fore.GREEN + '\ntest_numero_avistamientos_por_dia')
+    print(numero_avistamientos_por_dia(avistamientos))
+
+def test_año_mas_avistamientos_forma(fichero, forma):
+    avistamientos = leer_fichero(fichero)
+    print(Fore.BLUE + '\ntest_año_mas_avistamientos_forma')
+    print(año_mas_avistamientos_forma(avistamientos, forma))
+
+
+
+def test_estado_con_suma_top_n_duraciones_mas_larga(fichero, n):
+    avistamientos = leer_fichero(fichero)
+    print(Fore.CYAN + '\ntest_estado_con_suma_top_n_duraciones_mas_larga')
+    print(estado_con_suma_top_n_duraciones_mas_larga(avistamientos, n))
+    
+
 def main():
     fichero = './data/ovnis.csv'
+    # 1. Función de lectura de datos
     test_leer_fichero(fichero)
-    test_numero_avistamientos_fecha(fichero)
-    test_formas_estados(fichero)
-    test_avistamientos_cercanos_ubicacion(fichero)
-    test_duracion_maxima(fichero)
-    test_avistamiento_cercano_mayor_duracion(fichero)
-    test_avistamiento_mayor_duracion(fichero)
-    test_comentario_mas_largo(fichero, 2000, 'lights')
-    test_media_dias_entre_avistamientos(fichero, 2004)
-    test_hora_mas_avistamientos(fichero)
-    test_hora_mas_avistamientos2(fichero)
-    test_coordenadas_mas_avistamientos(fichero)
-    test_año_con_mayor_duracion_total_avistamientos(fichero)
+    lista = leer_fichero(fichero)
+    # 2. Operaciones de filtrado, conteo y suma
+    test_numero_avistamientos_fecha(lista, date(2010, 12, 3))
+    test_formas_estados(lista, {'in', 'nm'})
+    test_duracion_total(lista, 'in')
+    test_distancia()
+    test_avistamientos_cercanos_ubicacion(lista, ubicacion = Coordenadas(0, 0), distancia = 70)
+    # 3. Operaciones con máximos, mínimos y ordenación
+    test_avistamiento_mayor_duracion(lista, 'light')
+    test_avistamiento_cercano_mayor_duracion(lista, ubicacion = Coordenadas(0, 0), distancia = 70)
+    test_avistamientos_fechas(lista, date(2004, 10, 23), date(2004, 10, 24))
+    test_comentario_mas_largo(lista, 2000, 'lights')
+    test_media_dias_entre_avistamientos(lista, 1999)
+    # 4 Operaciones con Diccionarios
+    #test_avistamientos_por_fecha(lista) # No se puede testear porque el diccionario es muy grande
+    test_formas_por_mes(lista)
+    test_numero_avistamientos_por_año(lista)
+    test_coordenada_mas_avistamientos(lista)
+    test_hora_mas_avistamientos(lista)
+    test_longitud_media_comentarios_por_estado(lista)
+    #test_porc_avistamientos_por_forma(lista)
+
+
+    # test_hora_mas_avistamientos(fichero)
+    # test_hora_mas_avistamientos2(fichero)
+    # test_coordenadas_mas_avistamientos(fichero)
+    # test_año_con_mayor_duracion_total_avistamientos(fichero)
+    # test_numero_avistamientos_por_dia(fichero)
+    # test_año_mas_avistamientos_forma(fichero, 'light')
+    # test_longitud_media_comentarios_por_estado(fichero)
+    # test_estado_con_suma_top_n_duraciones_mas_larga(fichero, 15)
     Fore.RESET
     
 if __name__ == '__main__':
